20a1aff07ec8aaf1815ebf84aa8bf6f20bd08a16
vhkdiy <404924175@qq.com>
2017-12-23 14:25:09 +0800

    styled

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import '../assets/global.css'
+import styled from 'styled-components'
 
 export default ({ children }) => {
     return (
         <div>
-            <Link to='/'>首页</Link> &nbsp;
-            <Link to='/about'>关于</Link>
+            <Header>
+                <Link to='/'>首页</Link> &nbsp;
+                <Link to='/about'>关于</Link>
+            </Header>
             {children()}
         </div>
     )
 }
+
+const Header = styled.div`
+    line-height: 40px;
+    padding: 10px;
+    background: #00bcd4;
+    a {
+        color: white;
+    }
+`
